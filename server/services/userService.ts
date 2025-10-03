32cc58b22b08d57d02fcbd047fa2af4edc8e7af3
FamALouiz <fam@awadlouis.com>
2025-10-03 15:57:31 +0200

    fix: fixed linter issue for setup files

--- server/services/userService.ts
+++ server/services/userService.ts
@@ -1,10 +1,10 @@
-import UserModel from "../models/User";
+// import UserModel from "../models/User";
 
-export async function findAll() {
-  return UserModel.find().lean();
-}
+// export async function findAll() {
+//   return UserModel.find().lean();
+// }
 
-export async function create(data: any) {
-  const doc = new UserModel(data);
-  return doc.save();
-}
+// export async function create(data: any) {
+//   const doc = new UserModel(data);
+//   return doc.save();
+// }
