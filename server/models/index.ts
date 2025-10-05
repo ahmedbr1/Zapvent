d405a392526c24d220b56da830dbd0125ed319de
FamALouiz <fam@awadlouis.com>
2025-10-05 22:41:58 +0200

    feat: added models to index

--- server/models/index.ts
+++ server/models/index.ts
@@ -1,1 +1,4 @@
 export { default as User } from "./User";
+export { default as Court } from "./Court";
+export { default as Comment } from "./Comment";
+export { default as Rating } from "./Rating";
