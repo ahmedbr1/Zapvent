deffe54ecc9a1e4159a891f24ad071eefb1ffc96
FamALouiz <fam@awadlouis.com>
2025-10-05 22:41:01 +0200

    feat: updated user to use base model

--- server/models/User.ts
+++ server/models/User.ts
@@ -1,6 +1,7 @@
-import mongoose, { Document, Schema } from "mongoose";
+import mongoose, { Schema } from "mongoose";
+import { IBaseModel } from "./BaseModel";
 
-export interface IUser extends Document {
+export interface IUser extends IBaseModel {
   name: string;
   email: string;
 }
