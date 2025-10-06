5baccb0f6184ddbeb02f8a48c39b722ac524d980
FamALouiz <fam@awadlouis.com>
2025-10-06 21:48:42 +0200

    fix: linting for temp functions

--- server/services/userService.ts
+++ server/services/userService.ts
@@ -4,6 +4,8 @@ export async function findAll() {
   return UserModel.find().lean();
 }
 
+// TODO: Replace 'any' with a proper type/interface for user data
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function create(data: any) {
   const doc = new UserModel(data);
   return doc.save();
