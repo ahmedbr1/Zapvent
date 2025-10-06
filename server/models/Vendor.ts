f2f9f6198af34451c803e29219dffc30d96b50e8
ahmedbr1 <ahmed.sherif0120055942@gmail.com>
2025-10-06 11:12:54 +0200

    Revert "enforced camelCase"
    This reverts commit f67963c80e103b8cd43074cfffe81eacd9f58cea.

--- server/models/Vendor.ts
+++ server/models/Vendor.ts
@@ -36,9 +36,8 @@ const vendorSchema = new mongoose.Schema({
   boothLocation?: string;
   boothStartTime?: Date;
   boothEndTime?: Date;
-  // In the IVendor interface (around lines 27–28)
-  namesOfMembers?: string; // A URL to a document containing the names of members
-  loyaltyForum: string;    // URL containing the forum link
+  NamesOfMembers?: string; // A URL to a document containing the names of members
+  LoyltyForum: string; // URL containing the forum link
 }
 const vendorSchema = new Schema<IVendor>(
   {
