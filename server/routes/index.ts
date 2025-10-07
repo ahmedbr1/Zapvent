937cb7fe2ca058fe7b24f0b3794c8eb8c3aafa41
ahmedbr1 <ahmed.sherif0120055942@gmail.com>
2025-10-07 23:55:42 +0200

    added getAllEvents
    also added vendors list to Event

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import userRoutes from "./userRoutes";
 import loginRoutes from "./loginRoutes";
+import eventRoutes from "./eventRoutes";
 
 const api = Router();
 api.use("/users", userRoutes);
 api.use("/auth", loginRoutes);
+api.use("/events", eventRoutes);
 
 export default api;
