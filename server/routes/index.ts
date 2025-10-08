69de1f3467d023aeaffbbb9225a123de02b494cf
XENON-CYBER <yusufatif23@gmail.com>
2025-10-09 01:51:21 +0200

    added the view all the courts on campus with date time avaliblity

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import userRoutes from "./userRoutes";
 import loginRoutes from "./loginRoutes";
+import courtRoutes from "./courtRoutes";
 
 const api = Router();
 api.use("/users", userRoutes);
 api.use("/auth", loginRoutes);
+api.use("/courts", courtRoutes); 
 
 export default api;
