6c832a4fa1fa2fd25d74258f638c8c3063a2669d
FamALouiz <fam@awadlouis.com>
2025-10-09 23:19:43 +0200

    feat: added admin creation and deletion endpoints

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -3,11 +3,13 @@ import userRoutes from "./userRoutes";
 import loginRoutes from "./loginRoutes";
 import eventRoutes from "./eventRoutes";
 import gymSessionRoutes from "./gymSessionRoutes";
+import adminRoutes from "./adminRoutes";
 
 const api = Router();
 api.use("/users", userRoutes);
 api.use("/auth", loginRoutes);
 api.use("/events", eventRoutes);
 api.use("/gym-sessions", gymSessionRoutes);
+api.use("/admin", adminRoutes);
 
 export default api;
