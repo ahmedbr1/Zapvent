2803563271c47784a4c32f31982b18c051d2f7e8
Yusuf Atif <67690132+XENON-CYBER@users.noreply.github.com>
2025-10-12 21:58:09 +0200

    Feature/view the gym schedule (#25)
    * Added create a gym session with the following details: date, time, duration, type (e.g. yoga, pilates, aerobics, Zumba, cross circuit, kick-boxing), max number of participants
    
    * view the gym schedule for each month's sessions (e.g. yoga, pilates, aerobics, Zumba, cross circuit, kick-boxing)

--- server/routes/gymSessionRoutes.ts
+++ server/routes/gymSessionRoutes.ts
@@ -1,9 +1,15 @@
 import express from "express";
-import { cancelGymSessionController } from "../controllers/gymSessionController";
+import { cancelGymSessionController, createGymSessionController, viewGymScheduleByMonthController } from "../controllers/gymSessionController";
 
 const router = express.Router();
 
+// POST /api/gym-sessions
+router.post("/", createGymSessionController);
+
 // DELETE /api/gym-sessions/:id
 router.delete("/:id", cancelGymSessionController);
+
+// GET /api/gym-sessions/schedule?year=2025&month=10
+router.get("/schedule", viewGymScheduleByMonthController);
 
 export default router;
