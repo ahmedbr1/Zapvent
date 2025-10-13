cdcd6be1d33361b80efd1c9856cef00cb37a684c
Nourhan Shata <nourhan.islam10f@gmail.com>
2025-10-13 17:34:29 +0200

    fixed routing issue resolving code rabbit

--- server/routes/adminRoutes.ts
+++ server/routes/adminRoutes.ts
@@ -15,10 +15,10 @@ router.post("/", adminController.createAdmin.bind(adminController));
 router.get("/", adminController.getAllAdmins.bind(adminController));
 router.get("/:id", adminController.getAdminById.bind(adminController));
 
-router.delete("/:id", adminController.deleteAdmin.bind(adminController));
 router.delete(
   "/comments/:commentId",
   adminController.deleteComment.bind(adminController)
 );
+router.delete("/:id", adminController.deleteAdmin.bind(adminController));
 
 export default router;
