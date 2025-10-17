2d837adb0b5e8bd98578fddc6a7c5389c0acdf02
FamALouiz <fam@awadlouis.com>
2025-10-17 21:52:18 +0200

    feat: added jest setup

---
+++ jest.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  verbose: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coveragePathIgnorePatterns: ["/node_modules/"],
+  testMatch: ["**/tests/**/*.ts", "**/?(*.)+(spec|test).ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
+  extensionsToTreatAsEsm: [".ts"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+};
+
+export default config;
