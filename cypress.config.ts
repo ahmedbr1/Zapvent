43ec058c415572878b916a0bd0ea9835398985b4
Ahmed Kirat <ahmedkirat@Ahmeds-MacBook-Pro.local>
2025-10-18 12:42:58 +0200

    Added Setup Cypress

---
+++ cypress.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:3000", // 👈 change this to your app URL if different
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}", // 👈 where test files are
+    supportFile: "cypress/support/e2e.ts", // 👈 global setup (auto-created)
+    video: false, // disable video recordings (optional)
+    screenshotOnRunFailure: true,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      // e.g. on('task', { log: (message) => console.log(message) });
+    },
+  },
+  viewportWidth: 1280,
+  viewportHeight: 800,
+});
