cce7a07107bcfa7fbf0efaf1541981cce614eaf7
Nourhan Shata <nourhan.islam10f@gmail.com>
2025-11-11 14:19:37 +0100

    fix2

--- server/controllers/pollController.ts
+++ server/controllers/pollController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { Response } from "express";
 import type { AuthRequest } from "../middleware/authMiddleware";
 import {
   createVendorBoothPoll,
